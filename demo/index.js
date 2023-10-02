46a7ae29e21c47005129e718f34dbb5ff4755a32
wossenfekadie1993 <107991027+wossenfekadie1993@users.noreply.github.com>
2023-10-02 10:19:10 +0300

    updated

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+const fs =require('fs')
+
+// reading file
+// fs.readFile('./blog.txt',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// })
+// writing file
+// fs.writeFile('./blog1.txt','hello world',()=>{
+//     console.log("the file already written")
+// })
+
+
+// function greeting(name){
+//     console.log(`hello ${name} ,how are u doing`);
+// }
+// setTimeout(()=>{
+//    const  name='wossen';
+//     greeting(name);
+//     clearInterval(int);
+// },5000);
+
+
+// const int=setInterval(()=>{
+//     console.log("in 1 second interval")
+// },1000)
+
+// console.log(global);
+// console.log(__dirname);
+// console.log(__filename);
